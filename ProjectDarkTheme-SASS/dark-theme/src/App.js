24781e7bcbed62457a6218ccf3e0eea7e19880a7
Ömer YAVAŞ <omeryavas.pro@gmail.com>
2022-12-22 10:00:59 +0300

    Dark Theme Project Started

---
+++ ProjectDarkTheme-SASS/dark-theme/src/App.js
@@ -0,0 +1,43 @@
+import data from './data';
+import { useState } from 'react';
+
+function App() {
+  const [theme, setTheme] = useState('light-theme');
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
+  document.documentElement.className = theme;
+
+  return (
+    <div>
+      <nav>
+        <div className="nav-center">
+          <h1>World Cup - Dark/Light Theme</h1>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return (
+            <article className="post">
+              <h2>{item.title}</h2>
+              <div className="post-info">
+                <span>{item.score}</span>
+              </div>
+              <p>{item.level}</p>
+            </article>
+          );
+        })}
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ ProjectDarkTheme-SASS/dark-theme/src/data.js
@@ -0,0 +1,28 @@
+const worldCup = [
+  {
+    id: 1,
+    title: 'Argentina - France',
+    level: 'final',
+    score: `3 - 3`,
+  },
+  {
+    id: 2,
+    title: 'Croatia - Morocco',
+    level: 'Play-off for third place',
+    score: `2 - 1`,
+  },
+  {
+    id: 3,
+    title: 'France - Moroco',
+    level: 'Semi-final',
+    score: `2 - 0`,
+  },
+  {
+    id: 4,
+    title: 'Argentina - Croatia',
+    level: 'Semi-final',
+    score: `3 - 0`,
+  },
+];
+
+export default worldCup;
